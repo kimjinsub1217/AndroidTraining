3c8ac47477b33d743c3d9ee4cdcd20bad68c4715
kimjinsub <hikm9985@naver.com>
2023-03-27 22:38:45 +0900

    날씨앱 프로젝트 생성

---
+++ fastcampusAndroid/weatherApp/app/src/main/java/com/example/weatherapp/Forecast.kt
@@ -0,0 +1,11 @@
+package com.example.weatherapp
+
+data class Forecast(
+    val forecastDate: String,
+    val forecastTime: String,
+
+    var temperature: Double = 0.0,
+    var sky: String = "",
+    var precipitation: Int = 0,
+    var precipitationType: String = ""
+)
