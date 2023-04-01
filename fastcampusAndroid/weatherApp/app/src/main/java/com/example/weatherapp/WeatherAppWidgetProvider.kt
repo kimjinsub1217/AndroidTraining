b49c735a0c5556fc74574e50376577c0b86d552d
kimjinsub <hikm9985@naver.com>
2023-04-01 22:05:34 +0900

    위젯만들기

---
+++ fastcampusAndroid/weatherApp/app/src/main/java/com/example/weatherapp/WeatherAppWidgetProvider.kt
@@ -0,0 +1,38 @@
+package com.example.weatherapp
+
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.widget.RemoteViews
+
+class WeatherAppWidgetProvider : AppWidgetProvider() {
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+        appWidgetIds.forEach { appWidgetId ->
+            // Create an Intent to launch ExampleActivity
+            val pendingIntent: PendingIntent = Intent(context, MainActivity::class.java)
+                .let { intent ->
+                    PendingIntent.getActivity(context, 1, intent, PendingIntent.FLAG_IMMUTABLE)
+                }
+
+            // Get the layout for the App Widget and attach an on-click listener
+            // to the button
+            val views: RemoteViews = RemoteViews(
+                context.packageName,
+                R.layout.widget_weather
+            ).apply {
+                setOnClickPendingIntent(R.id.testTextView, pendingIntent)
+            }
+
+            // Tell the AppWidgetManager to perform an update on the current app widget
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+}
