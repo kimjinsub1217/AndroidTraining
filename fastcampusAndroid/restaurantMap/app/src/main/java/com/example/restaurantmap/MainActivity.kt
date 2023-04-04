7f86115f4e80c6d265fbd91286939befe224b398
kimjinsub <hikm9985@naver.com>
2023-04-04 22:02:45 +0900

    네이버 지도 사용하기

--- fastcampusAndroid/restaurantMap/app/src/main/java/com/example/restaurantmap/MainActivity.kt
+++ fastcampusAndroid/restaurantMap/app/src/main/java/com/example/restaurantmap/MainActivity.kt
@@ -2,10 +2,74 @@ package com.example.restaurantmap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.restaurantmap.databinding.ActivityMainBinding
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraAnimation
+import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var naverMap: NaverMap
+    private var isMapInt = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.mapView.onCreate(savedInstanceState)
+
+        binding.mapView.getMapAsync(this)
+
     }
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.mapView.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+
+        binding.mapView.onLowMemory()
+    }
+
+    override fun onMapReady(mapObject: NaverMap) {
+        naverMap = mapObject
+        isMapInt = true
+
+        val cameraUpdate = CameraUpdate.scrollTo(LatLng(37.5666102, 126.9783881))
+            .animate(CameraAnimation.Easing)
+        naverMap.moveCamera(cameraUpdate)
+    }
+
+
 }
