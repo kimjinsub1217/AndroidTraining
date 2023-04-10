de1326495e7f45027f516f01f4065e4dbe0e5fef
kimjinsub <hikm9985@naver.com>
2023-04-10 20:34:53 +0900

    firebase auth login 설정하기

--- fastcampusAndroid/zenlyApp/app/src/main/java/com/example/zenlyapp/LoginActivity.kt
+++ fastcampusAndroid/zenlyApp/app/src/main/java/com/example/zenlyapp/LoginActivity.kt
@@ -1,14 +1,21 @@
 package com.example.zenlyapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.zenlyapp.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.KakaoSdk
 import com.kakao.sdk.common.model.ClientError
 import com.kakao.sdk.common.model.ClientErrorCause
 import com.kakao.sdk.user.UserApiClient
+import com.kakao.sdk.user.model.User
 
 class LoginActivity : AppCompatActivity() {
 
@@ -16,11 +23,11 @@ class LoginActivity : AppCompatActivity() {
     private val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
         if (error != null) {
             //로그인 실패
-            Log.e("loginActivity2", "login $error")
+            showErrorToast()
             error.printStackTrace()
         } else if (token != null) {
             //로그인 성공
-            Log.e("loginActivity", "login $token")
+            getKaKaoAccountInfo()
         }
     }
 
@@ -29,7 +36,7 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        KakaoSdk.init(this,"${resources.getString(R.string.NATIVE_APP_KEY2)}")
+        KakaoSdk.init(this, "${resources.getString(R.string.NATIVE_APP_KEY2)}")
         binding.kakaoTalkLoginButton.setOnClickListener {
             if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
                 //카카오톡 로그인
@@ -42,6 +49,13 @@ class LoginActivity : AppCompatActivity() {
                         UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
                     } else if (token != null) {
                         //로그인 성공
+                        if (Firebase.auth.currentUser == null) {
+                            //카카오톡에서 정보를 가져와서 파이어베이스 로그인
+                            getKaKaoAccountInfo()
+
+                        } else {
+                            navigateToMapActivity()
+                        }
                     }
                 }
             } else {
@@ -51,5 +65,78 @@ class LoginActivity : AppCompatActivity() {
         }
 
 
+    }
+
+    private fun showErrorToast() {
+        Toast.makeText(this, "사용자 로그인에 실패했습니다.", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun getKaKaoAccountInfo() {
+        UserApiClient.instance.me { user, error ->
+            if (error != null) {
+                showErrorToast()
+         
+            } else if (user != null) {
+                //사용자 정보 요청 성공
+                Log.e(
+                    "LoginActivity",
+                    "user : 회원번호 : ${user.id} / 이메일 : ${user.kakaoAccount?.email} / 닉네임 : ${user.kakaoAccount?.profile?.nickname} / 프로필 사진 : ${user.kakaoAccount?.profile?.thumbnailImageUrl}"
+                )
+                checkKaKaoUserData(user)
+            }
+        }
+    }
+
+    private fun checkKaKaoUserData(user: User) {
+        val kakaoEmail = user.kakaoAccount?.email.orEmpty()
+
+        if (kakaoEmail.isEmpty()) {
+            return
+        }
+
+        signInFirebase(user, kakaoEmail)
+    }
+
+    private fun signInFirebase(user: User, email: String) {
+        val uId = user.id.toString()
+        Firebase.auth.createUserWithEmailAndPassword(email, uId).addOnCompleteListener {
+
+            if (it.isSuccessful) {
+                updateFirebaseDatabase(user)
+            } else {
+                showErrorToast()
+            }
+        }.addOnFailureListener {
+            if (it is FirebaseAuthUserCollisionException) {
+                Firebase.auth.signInWithEmailAndPassword(email, uId)
+                    .addOnCompleteListener { result ->
+                        if (result.isSuccessful) {
+                            updateFirebaseDatabase(user)
+                        } else {
+                            showErrorToast()
+                        }
+                    }.addOnFailureListener { error ->
+                    error.printStackTrace()
+                    showErrorToast()
+                }
+            } else {
+                showErrorToast()
+            }
+        }
+    }
+
+    private fun updateFirebaseDatabase(user : User){
+        val uid = Firebase.auth.currentUser?.uid.orEmpty()
+        val personMap = mutableMapOf<String, Any>()
+        personMap["uid"] = uid
+        personMap["name"] = user.kakaoAccount?.profile?.nickname.orEmpty()
+        personMap["profilePhoto"] = user.kakaoAccount?.profile?.thumbnailImageUrl.orEmpty()
+        Firebase.database.reference.child("Person").child(uid).updateChildren(personMap)
+
+        navigateToMapActivity()
+    }
+
+    private fun navigateToMapActivity() {
+        startActivity(Intent(this, MapActivity::class.java))
     }
 }
