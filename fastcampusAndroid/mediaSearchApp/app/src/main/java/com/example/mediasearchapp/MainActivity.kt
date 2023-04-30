e7383d49bf85bc1b242a98f8af851e3789cc8178
kimjinsub1217 <hikm9985@naver.com>
2023-04-30 20:44:10 +0900

    검색 UI 만들기 1

--- fastcampusAndroid/mediaSearchApp/app/src/main/java/com/example/mediasearchapp/MainActivity.kt
+++ fastcampusAndroid/mediaSearchApp/app/src/main/java/com/example/mediasearchapp/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.mediasearchapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.SearchView
+import com.example.mediasearchapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply {
+            setContentView(root)
+            view = this@MainActivity
+        }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+
+        (menu.findItem(R.id.search).actionView as SearchView).apply {
+            setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String): Boolean {
+
+                    return false
+                }
+
+                override fun onQueryTextChange(newText: String): Boolean {
+
+
+                    return false
+                }
+            })
+        }
+
+        return true
+    }
+
 }
